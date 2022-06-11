40a7ce5121f88e715d779c705210e315a765c0c9
Arcfoz <ikramtauffiqulh@gmail.com>
2022-06-12 05:37:24 +0700

    remove variabel not used

--- script.js
+++ script.js
@@ -32,11 +32,9 @@ if (dark_Mode === "enable") {
   document.querySelector(".sec").classList.toggle("dark");
 }
 
-var lampu = 0;
-var kipas = 0;
 var data = {};
 const timeout = setTimeout(datajson, 2000);
-const link = "https://smart-temp.herokuapp.com/api"
+const link = "http://localhost/api";
 
 setInterval(function () {
   fetch(link)
@@ -48,20 +46,18 @@ setInterval(function () {
       document.getElementById("suhu").innerHTML = res.temperature.toString() + "°C";
       document.getElementById("kelembaban").innerHTML = res.humidity.toString() + "%";
       if (data.lampu == 1) {
-        lampu = 1;
         document.getElementById("lampu").innerHTML = value = "ON";
         document.getElementById("switchlampu").checked = true;
+        data.lampu == 1
       } else {
-        lampu = 0;
         document.getElementById("lampu").innerHTML = value = "OFF";
         document.getElementById("switchlampu").checked = false;
+        data.lampu == 0
       }
       if (res.kipas == 1) {
-        kipas = 1;
         document.getElementById("kipas").innerHTML = value = "ON";
         document.getElementById("switchkipas").checked = true;
       } else {
-        kipas = 0;
         document.getElementById("kipas").innerHTML = value = "OFF";
         document.getElementById("switchkipas").checked = false;
       }
@@ -75,11 +71,9 @@ function datajson() {
 function nilailampu(value) {
   if (value == true) {
     value = "ON";
-    lampu = 1;
     data.lampu = 1;
   } else {
     value = "OFF";
-    lampu = 0;
     data.lampu = 0;
   }
   document.getElementById("lampu").innerHTML = value;
@@ -88,11 +82,9 @@ function nilailampu(value) {
 function nilaikipas(value) {
   if (value == true) {
     value = "ON";
-    kipas = 1;
     data.kipas = 1;
   } else {
     value = "OFF";
-    kipas = 0;
     data.kipas = 0;
   }
   document.getElementById("kipas").innerHTML = value;
@@ -128,6 +120,6 @@ function postInfokipas() {
   });
 }
 
-// setInterval(function () {
-//   console.log(data);
-// }, 100);
+setInterval(function () {
+  console.log(data);
+}, 100);
